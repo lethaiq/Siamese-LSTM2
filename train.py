a44a327b6bb21f476ebe72322ccbad4a72c2cf0d
thai le <thai.le@ist.psu.edu>
2018-11-26 16:53:32 -0500

    updates

--- train.py
+++ train.py
@@ -31,6 +31,7 @@ max_seq_length = 20
 use_w2v = True
 
 train_df, embeddings = make_w2v_embeddings(train_df, embedding_dim=embedding_dim, empty_w2v=not use_w2v)
+pickle.dump([train_df, embeddings], open('../quora/data/embeddings_glove.pkl','wb'))
 
 # Split to train validation
 validation_size = int(len(train_df) * 0.1)
