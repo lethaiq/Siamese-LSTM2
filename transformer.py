f99a444d1e783e811608b26fd862d03c85396ddc
thai le <thai.le@ist.psu.edu>
2018-11-26 18:13:08 -0500

    updates

---
+++ transformer.py
@@ -0,0 +1,119 @@
+import tensorflow as tf
+import tensorflow_hub as hub
+from time import time
+import pandas as pd
+
+import matplotlib
+
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+from sklearn.model_selection import train_test_split
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+import tensorflow as tf
+
+from tensorflow.python.keras.models import Model, Sequential
+from tensorflow.python.keras.layers import Input, Embedding, LSTM, GRU, Conv1D, Conv2D, GlobalMaxPool1D, Dense, Dropout
+
+from util import make_w2v_embeddings
+from util import split_and_zero_padding
+from util import ManDist
+import pickle
+
+from keras.layers import Input, Lambda, Dense
+from keras.models import Model
+import keras.backend as K
+
+# File paths
+TRAIN_CSV = '../quora/data/train.csv'
+
+# Load training set
+train_df = pd.read_csv(TRAIN_CSV)
+for q in ['question1', 'question2']:
+    train_df[q + '_n'] = train_df[q]
+
+# Make word2vec embeddings
+embedding_dim = 300
+max_seq_length = 20
+use_w2v = True
+
+# train_df, embeddings = make_w2v_embeddings(train_df, embedding_dim=embedding_dim, empty_w2v=not use_w2v)
+# pickle.dump([train_df, embeddings], open('../quora/data/embeddings_glove.pkl','wb'))
+train_df, embeddings = pickle.load(open('../quora/data/embeddings_glove.pkl','rb'))
+
+# Split to train validation
+validation_size = int(len(train_df) * 0.1)
+training_size = len(train_df) - validation_size
+
+X = train_df[['question1_n', 'question2_n']]
+Y = train_df['is_duplicate']
+
+X_train, X_validation, Y_train, Y_validation = train_test_split(X, Y, test_size=validation_size, random_state=22)
+
+X_train = split_and_zero_padding(X_train, max_seq_length)
+X_validation = split_and_zero_padding(X_validation, max_seq_length)
+
+# Convert labels to their numpy representations
+Y_train = Y_train.values
+Y_validation = Y_validation.values
+
+# Make sure everything is ok
+assert X_train['left'].shape == X_train['right'].shape
+assert len(X_train['left']) == len(Y_train)
+
+
+def UniversalEmbedding(x):
+    return embed(tf.squeeze(tf.cast(x, tf.string)), 
+        signature="default", as_dict=True)["default"]
+
+
+# --
+
+# Model variables
+gpus = 1
+batch_size = 1024 * gpus
+n_epoch = 50
+n_hidden = 50
+
+
+module_url = "https://tfhub.dev/google/universal-sentence-encoder-large/3"
+
+# Import the Universal Sentence Encoder's TF Hub module
+embed = hub.Module(module_url)
+
+
+x = Sequential()
+embedding = layers.Lambda(UniversalEmbedding,
+	output_shape=(embed_size,), trainable=False)
+x.add(embedding))
+x.add(LSTM(n_hidden))
+
+shared_model = x
+
+# The visible layer
+left_input = Input(shape=(max_seq_length,), dtype='int32')
+right_input = Input(shape=(max_seq_length,), dtype='int32')
+
+# Pack it all up into a Manhattan Distance model
+malstm_distance = ManDist()([shared_model(left_input), shared_model(right_input)])
+model = Model(inputs=[left_input, right_input], outputs=[malstm_distance])
+
+if gpus >= 2:
+    # `multi_gpu_model()` is a so quite buggy. it breaks the saved model.
+    model = tf.keras.utils.multi_gpu_model(model, gpus=gpus)
+model.compile(loss='mean_squared_error', optimizer=tf.keras.optimizers.Adam(), metrics=['accuracy'])
+model.summary()
+shared_model.summary()
+
+# Start trainings
+training_start_time = time()
+callbacks = [EarlyStopping(monitor='val_loss', patience=4)]
+malstm_trained = model.fit([X_train['left'], X_train['right']], Y_train,
+                           batch_size=batch_size, epochs=n_epoch,
+                           validation_data=([X_validation['left'], X_validation['right']], Y_validation, ), callbacks=callbacks)
+
+training_end_time = time()
+print("Training time finished.\n%d epochs in %12.2f" % (n_epoch,
+                                                        training_end_time - training_start_time))
+
+model.save('./data/SiameseLSTM.h5')
