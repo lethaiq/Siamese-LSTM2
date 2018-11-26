b4e43fce5d35ac823548f6884d4164cd148dc94b
thai le <thai.le@ist.psu.edu>
2018-11-26 18:54:09 -0500

    updates

--- transformer.py
+++ transformer.py
@@ -33,88 +33,20 @@ train_df = pd.read_csv(TRAIN_CSV)
 for q in ['question1', 'question2']:
     train_df[q + '_n'] = train_df[q]
 
-# Make word2vec embeddings
-embedding_dim = 300
-max_seq_length = 20
-use_w2v = True
-
-# train_df, embeddings = make_w2v_embeddings(train_df, embedding_dim=embedding_dim, empty_w2v=not use_w2v)
-# pickle.dump([train_df, embeddings], open('../quora/data/embeddings_glove.pkl','wb'))
-train_df, embeddings = pickle.load(open('../quora/data/embeddings_glove.pkl','rb'))
-
-# Split to train validation
-validation_size = int(len(train_df) * 0.1)
-training_size = len(train_df) - validation_size
-
 X = train_df[['question1_n', 'question2_n']]
 Y = train_df['is_duplicate']
 
 X_train, X_validation, Y_train, Y_validation = train_test_split(X, Y, test_size=validation_size, random_state=22)
 
-X_train = split_and_zero_padding(X_train, max_seq_length)
-X_validation = split_and_zero_padding(X_validation, max_seq_length)
+X_train = ["{} {}".format(i) for i in X_train.get_values()]
+X_validation = ["{} {}".format(i) for i in X_validation.get_values()]
 
 # Convert labels to their numpy representations
 Y_train = Y_train.values
 Y_validation = Y_validation.values
 
-# Make sure everything is ok
-assert X_train['left'].shape == X_train['right'].shape
-assert len(X_train['left']) == len(Y_train)
-
+embed = hub.Module(module_url)
 
 def UniversalEmbedding(x):
     return embed(tf.squeeze(tf.cast(x, tf.string)), 
         signature="default", as_dict=True)["default"]
-
-
-# --
-
-# Model variables
-gpus = 1
-batch_size = 1024 * gpus
-n_epoch = 50
-n_hidden = 50
-
-
-module_url = "https://tfhub.dev/google/universal-sentence-encoder-large/3"
-
-# Import the Universal Sentence Encoder's TF Hub module
-embed = hub.Module(module_url)
-
-x = Sequential()
-embedding = keras.layers.core.Lambda(UniversalEmbedding, output_shape=(300,), trainable=False)
-x.add(embedding)
-x.add(LSTM(n_hidden))
-
-shared_model = x
-
-# The visible layer
-left_input = Input(shape=(max_seq_length,), dtype='int32')
-right_input = Input(shape=(max_seq_length,), dtype='int32')
-
-# Pack it all up into a Manhattan Distance model
-malstm_distance = ManDist()([shared_model(left_input), shared_model(right_input)])
-model = Model(inputs=[left_input, right_input], outputs=[malstm_distance])
-
-
-with tf.Session() as session:
-    K.set_session(session)
-    session.run(tf.global_variables_initializer())  
-    session.run(tf.tables_initializer())
-
-    # Start trainings
-    training_start_time = time()
-    model.compile(loss='mean_squared_error', optimizer=tf.keras.optimizers.Adam(), metrics=['accuracy'])
-    model.summary()
-    shared_model.summary()
-    callbacks = [EarlyStopping(monitor='val_loss', patience=4)]
-    malstm_trained = model.fit([X_train['left'], X_train['right']], Y_train,
-                            batch_size=batch_size, epochs=n_epoch,
-                            validation_data=([X_validation['left'], X_validation['right']], Y_validation, ), callbacks=callbacks)
-
-    training_end_time = time()
-    print("Training time finished.\n%d epochs in %12.2f" % (n_epoch,
-                                                            training_end_time - training_start_time))
-
-    model.save('./data/SiameseLSTM.h5')
