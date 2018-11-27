c92cb6ce1f3f988dcb18d224751ab1430eeffd57
thai le <thai.le@ist.psu.edu>
2018-11-27 14:54:36 -0500

    updates

--- cnn_predict.py
+++ cnn_predict.py
@@ -48,14 +48,21 @@ training_size = len(train_df) - validation_size
 X = train_df[['question1_n', 'question2_n']]
 Y = train_df['is_duplicate']
 
-X_train, X_validation, Y_train, Y_validation = train_test_split(X, Y, test_size=validation_size, random_state=22)
+X_train, X_validation, Y_train, Y_validation = train_test_split(X, Y, test_size=0.2, random_state=22)
+X_validation, X_test, Y_validation, Y_test = train_test_split(X_validation, Y_validation, test_size=0.5, random_state=22)
+
+print(X_train.shape)
+print(X_validation.shape)
+print(X_test.shape)
 
 X_train = split_and_zero_padding(X_train, max_seq_length)
 X_validation = split_and_zero_padding(X_validation, max_seq_length)
+X_test = split_and_zero_padding(X_test, max_seq_length)
 
 # Convert labels to their numpy representations
 Y_train = Y_train.values
 Y_validation = Y_validation.values
+Y_test = Y_test.values
 
 # Make sure everything is ok
 assert X_train['left'].shape == X_train['right'].shape
@@ -63,25 +70,26 @@ assert len(X_train['left']) == len(Y_train)
 
 X_train = np.array([np.concatenate((X_train['left'][i], X_train['right'][i])) for i in range(len(X_train['left']))])
 X_validation = np.array([np.concatenate((X_validation['left'][i], X_validation['right'][i])) for i in range(len(X_validation['left']))])
+X_test = np.array([np.concatenate((X_test['left'][i], X_test['right'][i])) for i in range(len(X_test['left']))])
 # --
 
 model = tf.keras.models.load_model('./data/CNN_word2vec.h5')
 model.summary()
 
-prediction = model.predict(X_validation, verbose=1, batch_size=128)
-mse = mean_squared_error(Y_validation, prediction)
+prediction = model.predict(X_test, verbose=1, batch_size=128)
+mse = mean_squared_error(Y_test, prediction)
 prediction_int = prediction >= 0.5
 prediction_int = np.array(prediction_int).astype(int)
-acc = accuracy_score(Y_validation, prediction_int, normalize=True)
-f1 = f1_score(Y_validation, prediction_int, average='weighted') 
+acc = accuracy_score(Y_test, prediction_int, normalize=True)
+f1 = f1_score(Y_test, prediction_int, average='weighted') 
 print(mse, acc)
 print(f1)
 
-prediction = model.predict(X_train, verbose=1, batch_size=512)
-mse = mean_squared_error(Y_train, prediction)
-prediction_int = prediction >= 0.5
-prediction_int = np.array(prediction_int).astype(int)
-acc = accuracy_score(Y_train, prediction_int, normalize=True)
-f1 = f1_score(Y_train, prediction_int, average='weighted') 
-print(mse, acc)
-print(f1)
+# prediction = model.predict(X_train, verbose=1, batch_size=512)
+# mse = mean_squared_error(Y_train, prediction)
+# prediction_int = prediction >= 0.5
+# prediction_int = np.array(prediction_int).astype(int)
+# acc = accuracy_score(Y_train, prediction_int, normalize=True)
+# f1 = f1_score(Y_train, prediction_int, average='weighted') 
+# print(mse, acc)
+# print(f1)
