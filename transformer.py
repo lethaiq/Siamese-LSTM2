f607b7d4355fbc8feba920875f8a34d394a71acc
thai le <thai.le@ist.psu.edu>
2018-11-26 19:26:38 -0500

    updates

--- transformer.py
+++ transformer.py
@@ -63,10 +63,10 @@ def UniversalEmbedding(x):
 input_text = layers.Input(shape=(1,), dtype=tf.string)
 embedding = layers.Lambda(UniversalEmbedding, output_shape=(512,))(input_text)
 lstm = LSTM(50)(embedding)
-pred = layers.Dense(1, activation='softmax')(lstm)
+dense = layers.Dense(256, activation='relu')(embedding)
+pred = layers.Dense(1, activation='softmax')(dense)
 model = Model(inputs=[input_text], outputs=pred)
-model.compile(loss='binary_crossentropy', 
-	optimizer='adam', metrics=['accuracy'])
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 print(model.summary())
 
