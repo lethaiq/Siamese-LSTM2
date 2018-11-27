4f03a29931ebfd0c03348b160100863df1e088ef
thai le <thai.le@ist.psu.edu>
2018-11-26 19:29:33 -0500

    updates

--- transformer.py
+++ transformer.py
@@ -62,6 +62,8 @@ def UniversalEmbedding(x):
 
 input_text = layers.Input(shape=(1,), dtype=tf.string)
 embedding = layers.Lambda(UniversalEmbedding, output_shape=(512,))(input_text)
+print(embedding.summary())
+
 dense = LSTM(50)(embedding)
 # dense = layers.Dense(256, activation='relu')(embedding)
 pred = layers.Dense(1, activation='softmax')(dense)
